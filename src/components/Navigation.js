e1b17e6eda158378d43db1db46924c1631fe1e02
Elias Symons <eisymonsmusic@gmail.com>
2022-12-09 10:42:49 -0800

    updated ui

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,12 +2,27 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 
-import { Link, animateScroll as scroll } from "react-scroll";
+import Offcanvas from "react-bootstrap/Offcanvas";
+import { Link } from "react-scroll";
+import React from "react";
 
 function CollapsibleExample() {
+  const [menuOpen, setMenuOpen] = React.useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const handleClose = () => setMenuOpen(false);
+  const [windowWidth, setWindowWidth] = React.useState(window.innerWidth);
+
+  React.useEffect(() => {
+    window.addEventListener("resize", function () {
+      setWindowWidth(window.innerWidth);
+    });
+  }, []);
   return (
-    <Navbar collapseOnSelect expand="lg" className="navbar" variant="dark">
-      <Container className="nav-container">
+    <Navbar expand="lg" sticky="top" collapseOnSelect variant="dark">
+      <Container fluid>
         <Navbar.Brand className="nav-brand">
           <Link
             className="nav-brand"
@@ -22,49 +37,59 @@ function CollapsibleExample() {
             ES
           </Link>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav" className="nav-collapse">
-          <Nav className="ms-auto nav-link">
-            <Link
-              className="scroll-link"
-              activeClass="active"
-              to="/about"
-              spy={true}
-              smooth={true}
-              offset={-200}
-              duration={500}
-              delay={300}
-            >
-              About
-            </Link>
+        <Navbar.Toggle
+          aria-controls="offcanvasNavbar-expand-lg"
+          onClick={toggleMenu}
+        />
+        <Navbar.Offcanvas
+          id="offcanvasNavbar-expand-lg"
+          aria-labelledby="offcanvasNavbar-expand-lg"
+          placement="end"
+          restoreFocus={false}
+          show={menuOpen}
+          onHide={handleClose}
+        >
+          <Offcanvas.Header closeButton></Offcanvas.Header>
+          <Offcanvas.Body>
+            <Nav className="ms-auto">
+              <Link
+                className="scroll-link"
+                to="/about"
+                spy={true}
+                smooth={true}
+                offset={-200}
+                duration={500}
+                onClick={windowWidth < 992 && toggleMenu}
+              >
+                About
+              </Link>
 
-            <Link
-              className="scroll-link"
-              activeClass="active"
-              to="/projects"
-              spy={true}
-              smooth={true}
-              offset={-100}
-              duration={500}
-              delay={300}
-            >
-              Projects
-            </Link>
+              <Link
+                className="scroll-link"
+                to="/projects"
+                spy={true}
+                smooth={true}
+                offset={-100}
+                duration={500}
+                onClick={windowWidth < 992 && toggleMenu}
+              >
+                Projects
+              </Link>
 
-            <Link
-              className="scroll-link"
-              activeClass="active"
-              to="/contact"
-              spy={true}
-              smooth={true}
-              offset={0}
-              duration={500}
-              delay={300}
-            >
-              Contact
-            </Link>
-          </Nav>
-        </Navbar.Collapse>
+              <Link
+                className="scroll-link"
+                to="/contact"
+                spy={true}
+                smooth={true}
+                offset={0}
+                duration={500}
+                onClick={windowWidth < 992 && toggleMenu}
+              >
+                Contact
+              </Link>
+            </Nav>
+          </Offcanvas.Body>
+        </Navbar.Offcanvas>
       </Container>
     </Navbar>
   );
